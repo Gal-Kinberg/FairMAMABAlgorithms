693e8715d3cd93e871cf0a58013b34fc2b34577c
Gal Kinberg <Gankl33@gmail.com>
2022-08-16 13:13:28 +0300

    added test script for main

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from Agents import *
 from Arms import *
-from Environment import Environment
-
+from Environment import Environment, getUtilityMatrix
+from utils.NashSocialWelfare import *
 
 if __name__ == '__main__':
-    pass
+    nAgents = 5
+    nArms = 3
+    probabilities = np.random.random((nArms, nAgents))
+
+    arms = []
+    for arm in range(nArms):
+        arms.append(BernoulliArm(nAgents, probabilities=probabilities[arm]))
+
+    print(getUtilityMatrix(arms))
+    agents = RandomAgents(nAgents, nArms)
+
+    optRes = getOptimalNSW(getUtilityMatrix(arms))
+    optimalPolicy = optRes.x
+    print(optRes)
+
+    # nSimulations = 1
+    # simulationSteps = 100
+    # simulator = Environment(agents, arms)
+    # simulator.simulate(simulationSteps, nSimulations)
