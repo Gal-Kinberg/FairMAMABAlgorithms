d61c01856b04376770d7f8d8fe2d519ddcb0f0d1
Gal Kinberg <Gankl33@gmail.com>
2022-08-16 13:12:07 +0300

    add BernoulliArm

--- Arms.py
+++ Arms.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class Arm(ABC):
-    def __init__(self, nAgents):
+    def __init__(self, nAgents: int):
         self.nAgents = nAgents
         return
 
@@ -13,6 +13,18 @@ class Arm(ABC):
 
     @property
     @abstractmethod
-    def utilities(self):
+    def utilities(self) -> np.ndarray:
         """the true utilities of the arm for each agent"""
         pass
+
+
+class BernoulliArm(Arm):
+    def __init__(self, nAgents: int, probabilities):
+        super().__init__(nAgents)
+        self.probabilities = probabilities
+
+    def pull(self) -> np.ndarray:
+        return np.random.binomial(n=1, p=self.probabilities)
+
+    def utilities(self) -> np.ndarray:
+        return self.probabilities
