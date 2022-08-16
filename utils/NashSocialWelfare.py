cfff66ef628bc20245f807c4a8e6b1750d540006
Gal Kinberg <Gankl33@gmail.com>
2022-08-16 13:14:41 +0300

    basic NSW optimization methods
    basic SciPy optimization based on minimize() using constraints to optimize over the unit simplex.

--- utils/NashSocialWelfare.py
+++ utils/NashSocialWelfare.py
@@ -1,7 +1,23 @@
 import numpy as np
+from scipy.optimize import LinearConstraint, minimize
 
 from Arms import Arm
 
 
-def getNSW(arms: list[Arm], policy: np.ndarray):
-    pass
+def getNSW(policy: np.ndarray, utilityMatrix: np.ndarray):
+    """
+    :param policy: a 1D vector of probabilities for each arm
+    :param utilityMatrix: an nArms-by-nAgents matrix of utilities
+    returns the Nash Social Welfare for the given policy.
+    """
+    return np.prod(np.sum(policy[:, np.newaxis] * utilityMatrix, axis=0))
+
+
+def getOptimalNSW(utilityMatrix: np.ndarray):
+    nArms, nAgents = utilityMatrix.shape
+
+    # things to use:
+    simplexConst = LinearConstraint(np.ones(nArms), lb=1, ub=1)
+    positiveConst = LinearConstraint(np.eye(nArms), lb=0, ub=np.inf)
+    optimizationResult = minimize(fun=lambda x: -np.log(getNSW(x, utilityMatrix)), x0=1/nArms * np.ones(nArms), method='trust-constr', constraints=[simplexConst, positiveConst])
+    return optimizationResult
