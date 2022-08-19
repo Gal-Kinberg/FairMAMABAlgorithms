4b1cdc594b1bf87d8d86b6635ddd2d018806cf72
Gal Kinberg <Gankl33@gmail.com>
2022-08-19 16:17:56 +0300

    Added EpsilonGreedyAgents

--- Agents.py
+++ Agents.py
@@ -2,8 +2,6 @@ from abc import ABC, abstractmethod
 import numpy as np
 from utils.NashSocialWelfare import getNSW, getOptimalPolicy
 
-import utils.NashSocialWelfare
-
 
 class Agents(ABC):
     def __init__(self, nAgents: int, nArms: int):
@@ -61,4 +59,37 @@ class ExploreFirstAgents(Agents):
         self.t += 1
         if self.t == (self.nArms * self.explorationLength):
             self.estimatedOptimalPolicy = getOptimalPolicy(self.estimatedUtilityMatrix).x
+        return
+
+
+class EpsilonGreedyAgents(Agents):
+    def __init__(self, nAgents: int, nArms: int, epsilon):
+        super().__init__(nAgents, nArms)
+        self.timesPulled = np.zeros(self.nArms)  # number of times each arm was pulled
+        self.totalRewardsMatrix = np.zeros((self.nArms, self.nAgents))
+        self.t = 0
+        self.estimatedUtilityMatrix = np.zeros((self.nArms, self.nAgents))
+        self.estimatedOptimalPolicy = np.ones(self.nArms) / self.nArms  # initialize as a uniform random policy
+
+        # Greedy Variables
+        self.epsilon = epsilon
+        self.nextArmToPull = 0  # the next arm to pull for exploration
+
+    def getPolicy(self) -> np.ndarray:
+        # check if exploration or exploitation
+        if np.random.random() <= self.epsilon:  # Exploration
+            policy = np.zeros(self.nArms)
+            policy[self.nextArmToPull] = 1
+            self.nextArmToPull = np.mod(self.nextArmToPull + 1, self.nArms)
+        else:  # Exploitation
+            policy = getOptimalPolicy(self.estimatedUtilityMatrix).x
+        return policy
+
+    def observeReward(self, arm: int, reward) -> None:
+        # update estimated utility matrix
+        self.totalRewardsMatrix[arm] += reward
+        self.timesPulled[arm] += 1
+        self.estimatedUtilityMatrix[arm] = self.totalRewardsMatrix[arm] / self.timesPulled[arm]
+
+        self.t += 1
         return
