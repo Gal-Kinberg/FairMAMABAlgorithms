62d1b1976aaee41d661b328bea3c825e316dfd9e
Gal Kinberg <Gankl33@gmail.com>
2022-08-19 15:52:49 +0300

    Added ExploreFirstAgents

--- Agents.py
+++ Agents.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
 import numpy as np
+from utils.NashSocialWelfare import getNSW, getOptimalPolicy
+
+import utils.NashSocialWelfare
 
 
 class Agents(ABC):
@@ -26,4 +29,36 @@ class RandomAgents(Agents):
         return policy
 
     def observeReward(self, arm: int, reward) -> None:
+        return
+
+
+class ExploreFirstAgents(Agents):
+    def __init__(self, nAgents: int, nArms: int, explorationLength: int):
+        super().__init__(nAgents, nArms)
+        self.timesPulled = np.zeros(self.nArms)  # number of times each arm was pulled
+        self.totalRewardsMatrix = np.zeros((self.nArms, self.nAgents))
+        self.explorationLength = explorationLength
+        self.t = 0
+        self.estimatedUtilityMatrix = np.zeros((self.nArms, self.nAgents))
+        self.estimatedOptimalPolicy = np.ones(self.nArms) / self.nArms  # initialize as a uniform random policy
+
+    def getPolicy(self) -> np.ndarray:
+        explorationArm = int(np.floor(self.t / self.explorationLength))
+        if explorationArm < self.nArms:  # Exploration phase
+            policy = np.zeros(self.nArms)
+            policy[explorationArm] = 1
+        else:  # Exploitation
+            policy = self.estimatedOptimalPolicy
+
+        return policy
+
+    def observeReward(self, arm: int, reward) -> None:
+        # update estimated utility matrix
+        self.totalRewardsMatrix[arm] += reward
+        self.timesPulled[arm] += 1
+        self.estimatedUtilityMatrix[arm] = self.totalRewardsMatrix[arm] / self.timesPulled[arm]
+
+        self.t += 1
+        if self.t == (self.nArms * self.explorationLength):
+            self.estimatedOptimalPolicy = getOptimalPolicy(self.estimatedUtilityMatrix).x
         return
