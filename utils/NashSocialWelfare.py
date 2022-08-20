bugfix: minus sign in UCB optimization

utils/NashSocialWelfare.py
@@ -23,21 +23,23 @@ def getOptimalPolicy(utilityMatrix: np.ndarray):
 
     simplexConst = LinearConstraint(np.ones(nArms), lb=1, ub=1)
     positiveConst = LinearConstraint(np.eye(nArms), lb=0, ub=np.inf)
-    optimizationResult = minimize(fun=lambda x: -np.log(getNSW(x, utilityMatrix)), x0=initialPolicy, method='trust-constr', constraints=[simplexConst, positiveConst])
+    optimizationResult = minimize(fun=lambda x: -np.log(getNSW(x, utilityMatrix)), x0=initialPolicy,
+                                  method='trust-constr', constraints=[simplexConst, positiveConst])
     return optimizationResult.x
 
 
 def getOptimalUCBPolicy(utilityMatrix: np.ndarray, alpha, UCBs):
     nArms, nAgents = utilityMatrix.shape
 
-    initialPolicy = 1 / nArms * np.ones(nArms)
+    initialPolicy = np.ones(nArms) / nArms
 
     if getNSW(initialPolicy, utilityMatrix) == 0:  # nothing is known about the utilities right now
         return initialPolicy
 
     simplexConst = LinearConstraint(np.ones(nArms), lb=1, ub=1)
     positiveConst = LinearConstraint(np.eye(nArms), lb=0, ub=np.inf)
-    optimizationResult = minimize(fun=lambda x: getNSW(x, utilityMatrix) + alpha * np.sum(x * UCBs), x0=initialPolicy, method='trust-constr', constraints=[simplexConst, positiveConst])
+    optimizationResult = minimize(fun=lambda x: -getNSW(x, utilityMatrix) - alpha * np.sum(x * UCBs), x0=initialPolicy,
+                                  method='trust-constr', constraints=[simplexConst, positiveConst])
     return optimizationResult.x
 
 